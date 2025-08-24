29d132952d231c1968286a84ea8fb552e26c85d4
arcadyrajeev <bitmasteratj@gmail.com>
2025-08-25 05:22:23 +0530

    deployment

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 
-import React from "react";
-
 import Home from "./pages/home";
 import Projects from "./pages/projects";
 import About from "./pages/about";
