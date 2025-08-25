f011ef09143d616267a6fe4b9d8b14057d5d91ba
arcadyrajeev <bitmasteratj@gmail.com>
2025-08-25 19:37:27 +0530

    new

--- src/components/aboutSection.tsx
+++ src/components/aboutSection.tsx
@@ -1,5 +1,7 @@
 const AboutSection = () => {
-  return <section className="flex w-full h-[100dvh]"></section>;
+  return (
+    <section className="flex w-full h-[100dvh] bg-background-primary"></section>
+  );
 };
 
 export default AboutSection;
