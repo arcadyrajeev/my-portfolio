02f770fcfcbcba4ef93ed7a57c0055cb7e53461d
arcadyrajeev <bitmasteratj@gmail.com>
2025-08-29 18:26:30 +0530

    new

--- src/components/techCard.tsx
+++ src/components/techCard.tsx
@@ -7,11 +7,11 @@ interface TechProps {
 
 const TechCard: React.FC<TechProps> = ({ title, iconList }) => {
   return (
-    <div className="flex flex-col items-center md:items-start md:flex-row w-full h-fit items-center md:my-4">
-      <div className="flex w-full md:w-[40%] justify-center md:justify-start px-8 fontheading  text-[2rem] md:text-[10vw]  tracking-tighter leading-40 text-accent">
+    <div className="flex lg:py-30 flex-col items-center md:items-start md:flex-row w-full h-fit items-center md:my-4">
+      <div className="flex w-full md:w-[40%] justify-center md:justify-start text-light px-8 fontheading  text-[2rem] md:text-[10vw]  tracking-tighter leading-40 text-accent">
         {title}
       </div>
-      <div className="grid grid-cols-4 md:grid-cols-5 gap-2 md:gap-8">
+      <div className="grid grid-cols-4 md:grid-cols-5 gap-2 md:gap-8 lg:pl-10">
         {iconList.map((icon) => (
           <div className="flex flex-col gap-1 md:gap-4 items-center w-15  h-20 md:w-24 md:h-32 p-4">
             <img src={icon.iconLink} />
